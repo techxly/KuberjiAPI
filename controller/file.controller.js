d1d9c7c37ef021d6fd220b409d1ac468ffb9e1e1
AnilGoswami <anilgoswami1987@gmail.com>
2023-08-27 14:43:16 +0530

    Delete controller directory

--- controller/file.controller.js
+++
@@ -1,9 +0,0 @@
-const { uploadImage, data } = require('../services/file.services');
-
-exports.uploadImage = uploadImage;
-exports.upload = (req, res) => {
-
-    res.status(200).json({
-        fileData: data
-    })
-} 
