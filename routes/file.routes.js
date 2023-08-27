a1f85117e28ccc38d5fedfb80ced5677aaa5b089
AnilGoswami <anilgoswami1987@gmail.com>
2023-08-27 14:43:37 +0530

    Delete routes directory

--- routes/file.routes.js
+++
@@ -1,6 +0,0 @@
-module.exports = app => {
-    const router = require("express").Router();
-    const { uploadImage, upload } = require('../controller/file.controller');
-    router.post('/postImages', uploadImage, upload);
-    app.use('/api', router);
-}
--- routes/imageReco.routes.js
+++
@@ -1,6 +0,0 @@
-module.exports = app => {
-    const router = require("express").Router();
-    const faceController = require('../controller/imageReco.controller');
-    router.post('/matchFace', faceController.faceReco);
-    app.use('/api', router);
-}
